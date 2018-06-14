2f4a6181e682a1ac1f6e191c9f893a2a402aab5d
Het9000 <satasiyahet@gmail.com>
2018-06-14 09:44:34 -0700

    Update Pifunctions.py
    I just cleaned up the code
    Minor Bug fixes

--- Pifunctions.py
+++ Pifunctions.py
@@ -4,11 +4,12 @@ This is the function versrion, the one that uses functions.
 global pic,lnum
 def picalc(n):
     global pic
-    pic = n*(math.sqrt((1/2)-math.cos(360/n))
-    #return pic
+    pic = n*(math.sqrt((1/2)-math.cos(360/n)))
+    print("Time from start:", time()-start)
+    return pic
 
 
-##print("Time from start:", time()-start)
-
+"""
 while True:
     print("HI")
+"""
