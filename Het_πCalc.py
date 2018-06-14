663fdb6ae3585d16273ba165ee36b25ce193bd31
Het9000 <satasiyahet@gmail.com>
2018-06-14 09:36:21 -0700

    These are the files I've already made

---
+++ Het_πCalc.py
@@ -0,0 +1,29 @@
+from time import time
+import math
+lnum = 0
+pic = 0
+start = time()
+##while True:
+##    global a,lnum
+##    while lnum < 1001:
+##        pi = a*(math.sqrt((1/2)-math.cos(360/a)))
+##        a += 1
+##        lnum += 1
+##    print("Here is pi:",pi)
+##    print("Number of repetitions:",a)
+##    print("Time Elasped from the beginning",time()-start)
+################################################################################
+"""
+This is the function versrion, the one that uses functions.
+"""
+benchmarks = set()
+benchmarks = [1,10,100,1000,10000,100000,1000000,10000000]
+def picalc(n):
+    global pic
+    pic = n*(math.sqrt((1/2)-math.cos(360/n)))
+    return int(pic)
+
+
+#print("Time from start:", time()-start)
+for i in range(len(benchmarks)):
+    print(picalc(benchmarks[i]))
---
+++ Pifunctions.py
@@ -0,0 +1,14 @@
+"""
+This is the function versrion, the one that uses functions.
+"""
+global pic,lnum
+def picalc(n):
+    global pic
+    pic = n*(math.sqrt((1/2)-math.cos(360/n))
+    #return pic
+
+
+##print("Time from start:", time()-start)
+
+while True:
+    print("HI")
